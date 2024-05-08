dd2c127f6a64fa903352abb5bfe8f2a04ea3ffea
luislopez-developer <luislopezhuari27@gmail.com>
2024-05-07 20:55:31 -0500

    Funcionamiento basico de login

---
+++ src/main/java/Modelo/Usuario.java
@@ -0,0 +1,28 @@
+package Modelo;
+
+public class Usuario {
+    private String email;
+    private String password;
+
+    public Usuario(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters y setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
