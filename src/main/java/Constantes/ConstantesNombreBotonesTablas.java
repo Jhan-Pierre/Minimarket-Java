167118f7dbc2217b9f0f05480fe5f06141d460fa
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-05-11 17:49:23 -0500

    Se añadio als contantes para los botones de las tablas

---
+++ src/main/java/Constantes/ConstantesNombreBotonesTablas.java
@@ -0,0 +1,7 @@
+package Constantes;
+
+public class ConstantesNombreBotonesTablas {
+    public static final String VER_DETALLES = "ver detalles";
+    public static final String ELIMINAR = "Eliminar";
+    public static final String EDITAR = "Editar";          
+}
