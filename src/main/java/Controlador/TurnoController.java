99c45925ae6dceee475b079ad2a40957b42496a5
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-05-11 11:12:39 -0500

    se añadio a turno, estado y rol, que la opcion por defecto sea selecionar

--- src/main/java/Controlador/TurnoController.java
+++ src/main/java/Controlador/TurnoController.java
@@ -15,8 +15,13 @@ public class TurnoController {
     
     public void cargarTurnosEnComboBox(JComboBox<Turno> cboTurno) {
         cboTurno.removeAllItems(); // Limpiar el ComboBox
+
+        // Crear y agregar el ítem inicial para 'Seleccionar turno'
+        Turno seleccionarTurno = new Turno(0, "Seleccionar turno");
+        cboTurno.addItem(seleccionarTurno);
+
+        // Cargar el resto de los turnos desde la base de datos
         List<Turno> listaTurnos = modelo.listarTurno();
-        System.out.println(listaTurnos);
         for (Turno turno : listaTurnos) {
             cboTurno.addItem(turno);
         }
