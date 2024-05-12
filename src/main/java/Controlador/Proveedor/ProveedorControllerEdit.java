037bd376a69c3306bc409fbfe9a652a9d61aedfc
jeanci-jb <jeanci.99jb@gmail.com>
2024-05-11 22:50:29 -0500

    PanelProveedorEdit ProveedorControllerCreate ProveedorControllerEdit ProveedorControllerList ProveedorControllerDelete

--- src/main/java/Controlador/Proveedor/ProveedorControllerEdit.java
+++ src/main/java/Controlador/Proveedor/ProveedorControllerEdit.java
@@ -1,68 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controlador.Proveedor;
 
-/**
- *
- * @author jean carlos
- */
-import Modelo.Estado;
+import Modelo.CRUDProveedor;
 import Modelo.Proveedor;
-//import vista.PanelProveedorEditar;
 
 public class ProveedorControllerEdit {
-//    private ProveedorModelo proveedorModelo;
-//    private PanelProveedorEditar vistaProveedorEditar;
-//    private Long idProveedor;
-//
-//    public ProveedorControllerEdit() {
-//        this.proveedorModelo = new ProveedorModelo();
-//    }
-//
-//    public void setVistaProveedorEditar(PanelProveedorEditar vistaProveedorEditar) {
-//        this.vistaProveedorEditar = vistaProveedorEditar;
-//    }
-//
-//    public void setIdProveedor(Long idProveedor) {
-//        this.idProveedor = idProveedor;
-//    }
-//
-//    public void cargarDatosProveedor() {
-//        Proveedor proveedor = proveedorModelo.obtenerProveedorPorId(idProveedor);
-//        if (proveedor != null) {
-//            vistaProveedorEditar.txtNombre.setText(proveedor.getNombre());
-//            vistaProveedorEditar.txtRUC.setText(proveedor.getRuc());
-//            vistaProveedorEditar.txtDescripcion.setText(proveedor.getDescripcion());
-//            vistaProveedorEditar.txtTelefono.setText(proveedor.getTelefono());
-//            vistaProveedorEditar.txtCorreo.setText(proveedor.getCorreo());
-//            vistaProveedorEditar.txtDireccion.setText(proveedor.getDireccion());
-//
-//            // Establecer el estado seleccionado en el ComboBox
-//            for (int i = 0; i < vistaProveedorEditar.cboEstado.getItemCount(); i++) {
-//                Estado estado = (Estado) vistaProveedorEditar.cboEstado.getItemAt(i);
-//                if (estado.getId() == proveedor.getIdEstado()) {
-//                    vistaProveedorEditar.cboEstado.setSelectedIndex(i);
-//                    break;
-//                }
-//            }
-//        }
-//    }
-//
-//    public void guardarCambios() {
-//        String nombre = vistaProveedorEditar.txtNombre.getText();
-//        String ruc = vistaProveedorEditar.txtRUC.getText();
-//        String descripcion = vistaProveedorEditar.txtDescripcion.getText();
-//        String telefono = vistaProveedorEditar.txtTelefono.getText();
-//        String correo = vistaProveedorEditar.txtCorreo.getText();
-//        String direccion = vistaProveedorEditar.txtDireccion.getText();
-//
-//        int idEstado = ((Estado) vistaProveedorEditar.cboEstado.getSelectedItem()).getId();
-//
-//        proveedorModelo.actualizarProveedor(idProveedor, nombre, ruc, descripcion, telefono, correo, direccion, idEstado);
-//        
-//        // Aquí deberías tener un método para cerrar la ventana de edición
-//    }
+    private final CRUDProveedor proveedorModelo;
+    
+    public ProveedorControllerEdit(){
+        this.proveedorModelo = new CRUDProveedor();
+    }
+    
+    public Proveedor buscarProveedorPorCodigo(Long id){
+        return proveedorModelo.buscarProveedorPorCodigo(id);
+    }
+    
+    public void editarProveedor(Long id, String nombre, String ruc, String descripcion, String telefono, String correo, String direccion, int idEstado) { 
+        proveedorModelo.editarProveedor(id, nombre, ruc, descripcion, telefono, correo, direccion, idEstado);
+    }
+    
 }
-
+    
