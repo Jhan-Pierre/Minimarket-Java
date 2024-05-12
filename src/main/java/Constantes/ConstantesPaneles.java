e2e7d2ab527632b3b153628f22c15ad73f67e062
User <User@192.168.1.6>
2024-05-11 23:29:03 -0500

    Creacion de constante Producto

--- src/main/java/Constantes/ConstantesPaneles.java
+++ src/main/java/Constantes/ConstantesPaneles.java
@@ -7,7 +7,6 @@ public class ConstantesPaneles {
     public static final String PANEL_USUARIO_CREAR = "UsuarioCrear";
     public static final String PANEL_USUARIO_EDITAR = "UsuarioEditar";
     public static final String PANEL_USUARIO_SHOW = "UsuarioMostrar";
-    public static final String PANEL_PRODUCTO = "Producto";
     public static final String PANEL_PEDIDO = "Pedido";
     
     public static final String PANEL_VENTA = "Venta";
@@ -17,4 +16,10 @@ public class ConstantesPaneles {
     public static final String PANEL_CATEGORIA_CREAR = "CategoriaCrear";
     public static final String PANEL_CATEGORIA_EDITAR = "CategoriaEditar";
     public static final String PANEL_CATEGORIA_SHOW = "CategoriaMostrar";
+    
+    public static final String PANEL_PRODUCTO = "Producto";
+    public static final String PANEL_PRODUCTO_CREAR = "ProductoCrear";
+    public static final String PANEL_PRODUCTO_EDITAR = "ProductoEditar";
+    public static final String PANEL_PRODUCTO_SHOW = "ProductoMostrar";
+    
 }
