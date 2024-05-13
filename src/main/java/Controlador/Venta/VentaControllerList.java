7e4f5a4e25b856da47cf051e7f37679ec1d61b4c
LuisLopez-developer <156825396+LuisLopez-developer@users.noreply.github.com>
2024-05-13 03:03:06 -0500

    Se limito el eliminar, editar y ver detalles de las ventas de acuerdo a los permisos

--- src/main/java/Controlador/Venta/VentaControllerList.java
+++ src/main/java/Controlador/Venta/VentaControllerList.java
@@ -3,21 +3,35 @@ package Controlador.Venta;
 import Modelo.CRUDventa;
 import Modelo.Venta;
 import java.util.List;
+import java.util.Set;
 import javax.swing.table.DefaultTableModel;
 import static Constantes.ConstantesNombreBotonesTablas.*;
 
 public class VentaControllerList {
 
     private final CRUDventa modelo;
+    private Set<String> permisosUsuario;
 
-    public VentaControllerList() {
+    public VentaControllerList(Set<String> permisosUsuario) {
         this.modelo = new CRUDventa();
+        this.permisosUsuario = permisosUsuario;
     }
     
     //Funcion para buscar de acuerdo el nombre del usuario que realizo venta
     public DefaultTableModel obtenerModeloTabla(String textoBusqueda) {
         List<Venta> listaVentas = this.modelo.buscarVentaPorUsuario(textoBusqueda);
-        String[] columnNames = {"ID","Usuario", "fecha_hora", "Impuesto", "Total", "Metodo", "Comprobante", VER_DETALLES, EDITAR, ELIMINAR};
+        // Define las columnas básicas
+        String[] columnNames = {"ID", "Usuario", "fecha_hora", "Impuesto", "Total", "Metodo", "Comprobante"};
+        // Agrega las columnas de acciones si el usuario tiene permisos
+        if (permisosUsuario.contains("buscar_venta")) {
+            columnNames = new String[]{"ID", "Usuario", "fecha_hora", "Impuesto", "Total", "Metodo", "Comprobante", VER_DETALLES};
+        }
+        if (permisosUsuario.contains("editar_venta")) {
+            columnNames = new String[]{"ID", "Usuario", "fecha_hora", "Impuesto", "Total", "Metodo", "Comprobante", VER_DETALLES, EDITAR};
+        }
+        if (permisosUsuario.contains("eliminar_venta")) {
+            columnNames = new String[]{"ID", "Usuario", "fecha_hora", "Impuesto", "Total", "Metodo", "Comprobante", VER_DETALLES, EDITAR, ELIMINAR};
+        }
         DefaultTableModel model = new DefaultTableModel(columnNames, 0);
 
         for (Venta venta : listaVentas) {
@@ -28,14 +42,20 @@ public class VentaControllerList {
                 venta.getImpuesto(),
                 venta.getTotal(),
                 venta.getMetodo_pago(),
-                venta.getComprobante(),
-                VER_DETALLES,
-                EDITAR,
-                ELIMINAR
+                venta.getComprobante()
             };
+            // Agrega las celdas de acciones si el usuario tiene permisos
+            if (permisosUsuario.contains("buscar_venta")) {
+                row = new Object[]{venta.getId(), venta.getUsuario(), venta.getFecha_hora(), venta.getImpuesto(), venta.getTotal(), venta.getMetodo_pago(), venta.getComprobante(), VER_DETALLES};
+            }
+            if (permisosUsuario.contains("editar_venta")) {
+                row = new Object[]{venta.getId(), venta.getUsuario(), venta.getFecha_hora(), venta.getImpuesto(), venta.getTotal(), venta.getMetodo_pago(), venta.getComprobante(), VER_DETALLES, EDITAR};
+            }
+            if (permisosUsuario.contains("eliminar_venta")) {
+                row = new Object[]{venta.getId(), venta.getUsuario(), venta.getFecha_hora(), venta.getImpuesto(), venta.getTotal(), venta.getMetodo_pago(), venta.getComprobante(), VER_DETALLES, EDITAR, ELIMINAR};
+            }
             model.addRow(row);
         }
         return model;
     }
-    
 }
