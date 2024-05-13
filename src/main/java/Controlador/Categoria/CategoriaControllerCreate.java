36703aea5f8a8484bf89fe1376b8a36a261f6616
Fabrizzio Zambrano <fabrizziozv.17@gmail.com>
2024-05-12 21:56:17 -0500

    categoria asdasd

--- src/main/java/Controlador/Categoria/CategoriaControllerCreate.java
+++ src/main/java/Controlador/Categoria/CategoriaControllerCreate.java
@@ -1,6 +1,15 @@
 package Controlador.Categoria;
 
+import Modelo.CRUDCategoria;
 
 public class CategoriaControllerCreate {
+    private final CRUDCategoria categoriaModelo;
     
+    public CategoriaControllerCreate() {
+        this.categoriaModelo = new CRUDCategoria();
+    }
+    
+    public String agregarCategoria(String nombre) {
+        return categoriaModelo.agregarCategoria(nombre);
+    }
 }
